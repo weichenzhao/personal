e4e0c93680c93939fd36caa7e19abc717aa9f54f
richard <richard@richardzhao.(none)>
2014-02-04 23:50:15 -0800

    hello world code from <The Linux Kernel Module Programming Guide>

---
+++ kernel_module/helloworld/hello-1.c
@@ -0,0 +1,24 @@
+/*
+ * Code from http://www.tldp.org/LDP/lkmpg/2.6/html/lkmpg.html#AEN121
+ */
+
+/*  
+ *  hello-1.c - The simplest kernel module.
+ */
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
+
+int init_module(void)
+{
+	printk(KERN_INFO "Hello world 1.\n");
+
+	/* 
+	 * A non 0 return means init_module failed; module can't be loaded. 
+	 */
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Goodbye world 1.\n");
+}
