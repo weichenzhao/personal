c969a26d16fbf8cdddb308598d17ade1c9eb3a20
root <root@localhost.(none)>
2014-03-03 13:13:57 +0800

    adding type judging

--- kernel_module/packet_filter/filter.c
+++ kernel_module/packet_filter/filter.c
@@ -30,23 +30,58 @@ unsigned int hook_for_pkt(unsigned int hooknum,
 	__be32 sip,dip;
 	sip = iph->saddr;  
 	dip = iph->daddr;
+
+	/* Standard well-defined IP protocols.  */
+	//enum {
+	//  IPPROTO_IP = 0,               /* Dummy protocol for TCP               */
+	//  IPPROTO_ICMP = 1,             /* Internet Control Message Protocol    */
+	//  IPPROTO_IGMP = 2,             /* Internet Group Management Protocol   */
+	//  IPPROTO_IPIP = 4,             /* IPIP tunnels (older KA9Q tunnels use 94) */
+	//  IPPROTO_TCP = 6,              /* Transmission Control Protocol        */
+	//  IPPROTO_EGP = 8,              /* Exterior Gateway Protocol            */
+	//  IPPROTO_PUP = 12,             /* PUP protocol                         */
+	//  IPPROTO_UDP = 17,             /* User Datagram Protocol               */
+	//  IPPROTO_IDP = 22,             /* XNS IDP protocol                     */
+	//  IPPROTO_DCCP = 33,            /* Datagram Congestion Control Protocol */
+	//  IPPROTO_RSVP = 46,            /* RSVP protocol                        */
+	//  IPPROTO_GRE = 47,             /* Cisco GRE tunnels (rfc 1701,1702)    */
+	//
+	//  IPPROTO_IPV6   = 41,          /* IPv6-in-IPv4 tunnelling              */
+	//
+	//  IPPROTO_ESP = 50,            /* Encapsulation Security Payload protocol */
+	//  IPPROTO_AH = 51,             /* Authentication Header protocol       */
+	//  IPPROTO_BEETPH = 94,         /* IP option pseudo header for BEET */
+	//  IPPROTO_PIM    = 103,         /* Protocol Independent Multicast       */
+	//
+	//  IPPROTO_COMP   = 108,                /* Compression Header protocol */
+	//  IPPROTO_SCTP   = 132,         /* Stream Control Transport Protocol    */
+	//  IPPROTO_UDPLITE = 136,        /* UDP-Lite (RFC 3828)                  */
+	//
+	//  IPPROTO_RAW    = 255,         /* Raw IP packets                       */
+	//  IPPROTO_MAX
+	//};
+
 	//iph=(*skb).network_header;
-	//if(iph->protocol == IPPROTO_TCP){
-	//	printk("TCP packet received %d, %d\n", skb->data - skb->head, skb->len);
-	//}
-	printk("Packet for source address: %d.%d.%d.%d destination address: %d.%d.%d.%d\n", NIPQUAD(sip), NIPQUAD(dip));
+	if(iph->protocol == IPPROTO_TCP)
+		printk("TCP packet from: %d.%d.%d.%d; to: %d.%d.%d.%d\n", NIPQUAD(sip), NIPQUAD(dip));
+	else if(iph->protocol == IPPROTO_ICMP)
+		printk("ICMP packet from: %d.%d.%d.%d; to: %d.%d.%d.%d\n", NIPQUAD(sip), NIPQUAD(dip));
+	else if(iph->protocol == IPPROTO_UDP)
+		printk("UDP packet from: %d.%d.%d.%d; to: %d.%d.%d.%d\n", NIPQUAD(sip), NIPQUAD(dip));
+	else
+		printk("ICMP packet received %d, %d\n", skb->data - skb->head, skb->len);
 	//printk("Hello packet! %d, %d\n", skb->data - skb->head, skb->len);
 	return NF_ACCEPT;
 }
 
 static int init_filter_if()
 {
-	printk("initializing the hooks!\n");
+	printk(KERN_INFO "initializing the hooks!\n");
 
 	/* remember which hook you specified */
 	nfho.hook = hook_for_pkt;
-	nfho.hooknum = 0;//NF_IP_PRE_ROUTING;
-	nfho.pf = PF_INET; /* ipv4 protocols */
+	nfho.hooknum = 0;			// NF_IP_PRE_ROUTING;
+	nfho.pf = PF_INET;			// ipv4 protocols
 	nfho.priority =NF_IP_PRI_FIRST;
 
 	nf_register_hook(&nfho);
