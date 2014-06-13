5f5e7ce17defbcfcaf1ddb3b779bae78eb710477
richard zhao <09zwcbupt@gmail.com>
2014-06-13 01:52:55 -0700

    add pcap version

---
+++ bpf/pcap_bpf.c
@@ -0,0 +1,24 @@
+#include <pcap.h>
+#include <pcap/bpf.h>
+#include <stdio.h>
+
+#define DLT_EN10MB 1
+
+int main(){
+    int snaplen = 65535;
+    pcap_t *pcap = pcap_open_dead( DLT_EN10MB, snaplen );
+    struct bpf_program prog;
+    int netmask = 0;
+    char *rule = "ip";
+    pcap_compile( pcap, &prog, rule, 0, netmask );
+    printf("len: %d\n", prog.bf_len);
+    int i=0;
+    for(i; i<prog.bf_len; i++){
+        printf("instruction: %2d %d %d 0x%04x\n", 
+                prog.bf_insns[i].code, 
+                prog.bf_insns[i].jt,
+                prog.bf_insns[i].jf,
+                prog.bf_insns[i].k);
+    }
+    return 0;
+}
